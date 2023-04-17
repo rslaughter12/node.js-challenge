def70f49f1fc97779f1ab6b3ac821415fc484ae1
rslaughter12 <rslaughter12@gmail.com>
2023-04-16 20:32:29 -0700

    readmetxt function added, default answers added

---
+++ README.md
@@ -0,0 +1,26 @@
+# My Current Project
+
+## Description
+
+No Discription
+
+## Table of Contents
+
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Credits
+
+No Other Contributors
+
+## License
+
+This project is licensed under the Apache License 2.0 license.
+
+## Questions
+
+If you have any questions, please feel free to contact me:
+
+- GitHub: No GitHub Username Provided
+- Email: User has not provided their email
--- index.js
+++ index.js
@@ -6,28 +6,39 @@
     {
         type: 'input',
         name: 'name',
-        message: 'what is your name?',
+        message: 'what is the name of your project?',
+        default: 'My Current Project',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'what is your project about?',
+        default: 'No Discription',
     },
     {
         type: 'input',
         name: 'credit',
         message: 'Do you have anyone that you need to give credit to?',
+        default: 'No Other Contributors',
     },
     {
         type: 'list',
         name: 'license',
         message: 'What license does your project use?',
-        choices: ['Apache License 2.0, GNU General Public License v3.0, MIT LicenseBSD, 2-Clause "Simplified" LicenseBSD, 3-Clause "New" or "Revised" LicenseBoost Software License 1.0, Creative Commons Zero v1.0 UniversalEclipse Public License 2.0, GNU Affero General Public License v3.0, GNU General Public License v2.0, GNU Lesser General Public License v2.1, Mozilla Public License 2.0, The Unlicense'],
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT LicenseBSD', '2-Clause "Simplified" LicenseBSD', '3-Clause "New" or "Revised" LicenseBoost Software License 1.0', 'Creative Commons Zero v1.0 Universal','Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+        default: 'No license',
     },
     {
         type: 'input',
         name: 'username',
         message: 'What is your GitHub username?',
+        default: 'No GitHub Username Provided',
     },
     {
         type: 'input',
         name: 'email',
         message: 'What is your email?',
+        default: 'User has not provided their email',
     },
  ])
  .then((answers) => {
@@ -38,3 +49,33 @@
         console.log('success!');
     });
  });
+
+ function readmetxt(answers) {
+    return `# ${answers.name}
+
+## Description
+
+${answers.description}
+
+## Table of Contents
+
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Credits
+
+${answers.credit}
+
+## License
+
+This project is licensed under the ${answers.license} license.
+
+## Questions
+
+If you have any questions, please feel free to contact me:
+
+- GitHub: ${answers.username}
+- Email: ${answers.email}
+`;
+}
